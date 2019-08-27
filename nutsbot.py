43754ea0705251a898acd55664255a2e3cf10c51
Marcos Marado <mindboosternoori@gmail.com>
2019-08-27 18:19:36 +0100

    updated so it works with both python2 and python3

--- nutsbot.py
+++ nutsbot.py
@@ -1,6 +1,6 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
-import telnetlib, sys, time, os, commands
+import telnetlib, sys, time, os
 from xml.dom import minidom   
 
 HOST = 'example.org'
@@ -9,19 +9,19 @@ MYNAME = 'username'
 MYPASS = 'password'
 
 telnet = telnetlib.Telnet(HOST, PORT)
-telnet.read_until("name:")
-telnet.write(MYNAME + '\n') # username
-telnet.read_until("sword:")
-telnet.write(MYPASS + '\n')  # password
-telnet.read_until("continue:")
-telnet.write('\n')         # press enter
-telnet.write('\n')
-telnet.write('\n')
+telnet.read_until(b'name:')
+telnet.write((MYNAME + '\n').encode('ascii')) # username
+telnet.read_until(b'sword:')
+telnet.write((MYPASS + '\n').encode('ascii')) # password
+telnet.read_until(b'continue:')
+telnet.write(('\n').encode('ascii'))          # press enter
+telnet.write(('\n').encode('ascii'))          # press enter
+telnet.write(('\n').encode('ascii'))          # press enter
 
 # examples of things to do:
-telnet.write(".bcast broadcasting something \n") # do this
-telnet.read_until("Are you sure about this") # wait until you receive this string
+telnet.write((".bcast broadcasting something \n").encode('ascii')) # do this
+telnet.read_until(b'Are you sure about this') # wait until you receive this string
 
 # exit
-telnet.write('.q\n')
+telnet.write(('.q\n').encode('ascii'))
 sock.close()
